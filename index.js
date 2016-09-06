5004f2c80e54f6a2543dd73c64ed946b2618bdb5
Eric Zhang <i@qinix.com>
2016-09-07 01:02:58 +0800

    Support using '$' for inline math and '$$' for display math, same as most of the markdown apps

--- index.js
+++ index.js
@@ -22,14 +22,32 @@ module.exports = {
                 end: "$$"
             },
             process: function(blk) {
-                var tex = blk.body;
-                var isInline = !(tex[0] == "\n");
+                var tex = blk.body.replace(/\\([^a-zA-Z0-9])/g, "$1");
+                var isInline = false;
                 var output = katex.renderToString(tex, {
                     displayMode: !isInline
                 });
 
                 return output;
             }
+        },
+        inline_math: {
+          shortcuts: {
+            parsers: ["markdown", "asciidoc", "restructuredtext"],
+            start: "$",
+            end: "$"
+          },
+          process: function(blk) {
+            console.log(blk);
+            var tex = blk.body.replace(/\\([^a-zA-Z0-9])/g, "$1");
+            console.log(tex);
+            var isInline = true;
+            var output = katex.renderToString(tex, {
+              displayMode: !isInline
+            });
+
+            return output;
+          }
         }
     }
 };
